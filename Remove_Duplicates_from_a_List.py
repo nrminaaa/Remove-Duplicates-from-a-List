f61c4f55eb18b550759faac38d27063e4d935e04
nrminaaa <abdullayevanermine141@gmail.com>
2025-04-02 11:47:22 +0400

    def funksiyasi ile

--- Remove_Duplicates_from_a_List.py
+++ Remove_Duplicates_from_a_List.py
@@ -1,19 +1,8 @@
-a=[1,2,2,3,1]
-m=[]
-b=a.count(1)
-c=a.count(2)
-d=a.count(3)
-if b>1:
-     a.remove(1)
-m.append(1)
-if c>1:
-       a.remove(2)
-m.append(2)
-if d>1:
-    a.remove(3)
-m.append(3)
-
-print(a,m)
-
-        
-
+def remove_duplicate(a):
+    h=[]
+    for i in a:
+        if i not in h:
+            h.append(i)
+    return h
+a=[1,2,1,3,3,4,4,5]
+print(remove_duplicate(a))
